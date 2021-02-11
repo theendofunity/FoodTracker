03f1b6db74741faf732fc7bc8d2fb3b7d364aada
theendofunity <dwd96@yandex.ru>
2021-02-11 19:36:32 +0300

    added deleting ability

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -17,8 +17,11 @@ class MealTableViewController: UITableViewController {
     var meals = [Meal]()
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
+        navigationItem.leftBarButtonItem = editButtonItem
+        
         loadSampleMeals()
         
     }
@@ -53,25 +56,22 @@ class MealTableViewController: UITableViewController {
     }
 
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
+
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            meals.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+
         }    
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
